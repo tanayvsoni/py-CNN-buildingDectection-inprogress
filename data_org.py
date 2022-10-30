316719a2659bf3def00f5e9174b2269f6797b605
Tanay Soni <tanayvsoni@gmail.com>
2022-10-30 17:52:26 -0600

    Data Orginization

---
+++ data_org.py
@@ -0,0 +1,23 @@
+from msilib.schema import Directory
+import os
+from PIL import Image
+
+def get_photos_buildings():
+    data_directory = './data'
+    input_img = 
+    
+    print('Enter info about image: ')
+    
+    number_of_buildings = ('')
+    
+    
+    
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    
+    
