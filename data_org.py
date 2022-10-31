229bc1154185ba4d39cc61cb409993a23ef785e7
Tanay Soni <tanayvsoni@gmail.com>
2022-10-30 18:59:35 -0600

    Data Orginization

--- data_org.py
+++ data_org.py
@@ -4,18 +4,31 @@ from PIL import Image
 
 def get_photos_buildings():
     data_directory = './data'
-    input_img = 
     
-    print('Enter info about image: ')
+    with open(data_directory + '/output_zip_info/image_data.csv','r') as csvfile:
+        file = csvfile.read().splitlines()
+        last_imgID = int(file[-1].split(',')[0][:-4])
+        
+          
+    img_names = os.listdir(data_directory+'/input_image')
     
-    number_of_buildings = ('')
-    
-    
-    
-
-
+    for img in img_names:
+        last_imgID += 1
+        while True:
+            try:
+                print(f'\nFor image {img} please enter relevent info:')
+                country_name = input('Enter name of country present in image: ').lower()
+                date = input('Enter date image was taken (DD/MM/YY): ')
+                [latitude, longitude] = input('Enter the lat,long coordinates of image: ').split(',')
+                num_building = int(input('Enter number of buildings present in image: '))
+                break
+            except: print('Invalid input. Try again')
+            
+        with open(data_directory + '/output_zip_info/image_data.csv','a') as csvfile:
+            csvfile.write(f'{last_imgID}.png,f{num_building},{country_name},{date},{latitude},{longitude}\n')
+            
 def main():
-    pass
+    get_photos_buildings()
 
 if __name__ == '__main__':
     main()
