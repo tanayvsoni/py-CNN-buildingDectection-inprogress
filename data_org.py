a77a3de81754f801eb02b71196b13a723f33f9ed
Tanay Soni <tanayvsoni@gmail.com>
2022-10-31 17:05:57 -0600

    Data Organization

--- data_org.py
+++ data_org.py
@@ -1,44 +1,47 @@
-
 import os
 import zipfile
 
 def org_files():
     data_directory = './data'
     
-    with open(data_directory + '/output_zip_info/image_data.csv','r') as csvfile:
+    with open(f'{data_directory}/output_zip_info/image_data.csv','r') as csvfile:
         file = csvfile.read().splitlines()
         last_imgID = int(file[-1].split(',')[0][:-4])
         
           
-    img_names = os.listdir(data_directory+'/input_image')
+    img_names = os.listdir(f'{data_directory}/input_image')
     
     for img in img_names:
         last_imgID += 1
         new_name = f'{last_imgID}.png'
-        os.rename(data_directory+f'/input_image/{img}',data_directory+f'/input_image/{new_name}')
+        os.rename(f'{data_directory}/input_image/{img}',f'{data_directory}/input_image/{new_name}')
     
         while True:
             try:
-                print(f'\nFor image {img} please enter relevent info:')
+                print(f'\nFor image {img} please enter relevent info:\n')
+                
                 country_name = input('Enter name of country present in image: ').lower()
                 date = input('Enter date image was taken (DD/MM/YY): ')
-                [latitude, longitude] = input('Enter the lat,long coordinates of image: ').split(',')
+                [latitude, longitude] = input('Enter the coordinates of image (lat,long): ').split(',')
                 num_building = input('Enter number of buildings present in image: ')
+                
+                if input('\nWould you like to edit inputs (y/n): ').lower() == 'y': raise
                 break
-            except: print('Invalid input. Try again')
             
-        with open(data_directory + '/output_zip_info/image_data.csv','a') as csvfile:
+            except: pass
+            
+        with open(f'{data_directory}/output_zip_info/image_data.csv','a') as csvfile:
             csvfile.write(f'{new_name},{num_building},{country_name},{date},{latitude},{longitude}\n')
         
-        with zipfile.ZipFile(data_directory + '/output_zip_info/output.zip','a') as zipf:
-            zipf.write(data_directory+f'/input_image/{new_name}',f'{new_name}')
-
-        os.remove(data_directory+f'/input_image/{new_name}')
-
-        
+        with zipfile.ZipFile(f'{data_directory}/output_zip_info/output.zip','a') as zipf:
+            zipf.write(f'{data_directory}/input_image/{new_name}',f'{new_name}')
             
+        os.remove(f'{data_directory}/input_image/{new_name}')
+                       
 def main():
-    org_files()
+    while True:
+        org_files()
+        if input('\nAll files have been stored, would you like to input again (y/n): ').lower() == 'n': break
 
 if __name__ == '__main__':
     main()
