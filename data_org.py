4a1a33b33a5c3baa3d6e7e04c456a8614ce49b68
Tanay Soni <tanayvsoni@gmail.com>
2022-11-01 20:13:21 -0600

    Data Organization

--- data_org.py
+++ data_org.py
@@ -10,12 +10,12 @@ def org_files(data_directory):
     """
     
     # Get last image ID that was used from csv file
-    with open(f'{data_directory}/output_zip_info/image_data.csv','r') as csvfile:
+    with open(f'{data_directory}/output/image_data.csv','r') as csvfile:
         file = csvfile.read().splitlines()
         last_imgID = int(file[-1].split(',')[0][:-4])
         
     # Get all input images (should be only 1 image ideally)      
-    img_names = os.listdir(f'{data_directory}/input_image')
+    img_names = os.listdir(f'{data_directory}/input')
     
     for img in img_names:
         # Create new image name by adding 1 to previous ID number
@@ -23,7 +23,7 @@ def org_files(data_directory):
         new_name = f'{last_imgID}.png'
         
         # Rename Image
-        os.rename(f'{data_directory}/input_image/{img}',f'{data_directory}/input_image/{new_name}')
+        os.rename(f'{data_directory}/input/{img}',f'{data_directory}/input/{new_name}')
     
         while True:
             try:
@@ -42,15 +42,15 @@ def org_files(data_directory):
             except: pass
         
         # Write data input csv file    
-        with open(f'{data_directory}/output_zip_info/image_data.csv','a') as csvfile:
+        with open(f'{data_directory}/output/image_data.csv','a') as csvfile:
             csvfile.write(f'{new_name},{num_building},{country_name},{date},{latitude},{longitude},{comments}\n')
         
         # Copy image into zip file
-        with zipfile.ZipFile(f'{data_directory}/output_zip_info/output.zip','a') as zipf:
-            zipf.write(f'{data_directory}/input_image/{new_name}',f'{new_name}')
+        with zipfile.ZipFile(f'{data_directory}/output/output.zip','a') as zipf:
+            zipf.write(f'{data_directory}/input/{new_name}',f'{new_name}')
         
         # Remove image from input folder    
-        os.remove(f'{data_directory}/input_image/{new_name}')
+        os.remove(f'{data_directory}/input/{new_name}')
                        
 if __name__ == '__main__':
     while True:
