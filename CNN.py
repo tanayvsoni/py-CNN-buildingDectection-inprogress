00b936c41e433b1a04b21cacbdaa90b868006430
Tanay Soni <tanayvsoni@gmail.com>
2022-11-04 17:30:28 -0600

    Data Organization

--- CNN.py
+++ CNN.py
@@ -7,16 +7,19 @@ import matplotlib
 from zipfile import ZipFile
 from PIL import Image
 from torchvision import transforms
+from torch.utils.data import random_split
 from matplotlib import pyplot as plt
 
 def get_data(data_directory):
     data_directory += '/output'
     
-    with ZipFile(data_directory+'/output.zip','r') as zip:
-        zip.extractall(path = data_directory+'/images')
-        
+    # Checks if there is any need to extract zip file
+    if os.listdir(data_directory+'/images') == []:  
+        with ZipFile(data_directory+'/output.zip','r') as zip:
+            zip.extractall(path = data_directory+'/images')
+             
     images = os.listdir(data_directory+'/images')
-    
+        
     with open(data_directory+'/image_data.csv') as csvfile:
         cf = csvfile.read().splitlines()
         
@@ -33,10 +36,6 @@ def get_data(data_directory):
         img = Image.open(f'{data_directory}/images/{images[i]}')
 
         dataset.append([convert_tensor(img)[:3,:,:], labels[i]])
-
-    tensor_img, label = dataset[0]
-    
-    print(tensor_img.shape)
     
     return dataset
 
@@ -44,10 +43,23 @@ def view_example(img,label):
     print(f'Label: {label}')
     plt.imshow(img.permute(1,2,0))
     plt.show()
+
+def split_data(test_size,random_seed,dataset):
     
+    torch.manual_seed(random_seed)
+    train_size = len(dataset) - test_size
+    
+    train_ds, test_ds = random_split(dataset, [train_size, test_size])
+    
+    return train_ds,test_ds
+
 def main():
     dataset = get_data('./data')
-    print(dataset[0])
+    
+    train_ds,test_ds = split_data(1000,69,dataset)
+    
+    print(len(train_ds), len(test_ds))
+    
 
 if __name__ == '__main__':
     main()
